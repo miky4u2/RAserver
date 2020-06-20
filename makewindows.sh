a93e14c60f39a30ffbce2a81e5af1cd8c9fd2c21
MIKY-WIN10\Miky <michael@dmtec.com.au>
2020-06-21 01:08:10 +1000

    missing dot in build line

--- makewindows.sh
+++ makewindows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#GOOS=windows GOARCH=amd64 go build -ldflags -H=windowsgui -o ./runtime/bin/server.exe  /server/server.go
+#GOOS=windows GOARCH=amd64 go build -ldflags -H=windowsgui -o ./runtime/bin/server.exe  ./server/server.go
 GOOS=windows GOARCH=amd64 go build -o ./runtime/bin/server.exe  ./server/server.go
